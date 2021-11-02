0bf05bd361769d9f0affaef83dc3451b6c279a81
jiexipeng <jiexipeng@kongfz.com>
2021-11-02 16:59:03 +0800

    ++

---
+++ login-jwt/src/main/java/com/clear/loginjwt/LoginJwtApplication.java
@@ -0,0 +1,16 @@
+package com.clear.loginjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiexipeng
+ */
+@SpringBootApplication
+public class LoginJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginJwtApplication.class, args);
+    }
+
+}
