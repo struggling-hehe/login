e0a90ccbc2304c88bd0419f91920c78a123b28bc
jiexipeng <jiexipeng@kongfz.com>
2021-11-16 17:34:52 +0800

    ++

--- login-jwt/src/main/java/com/clear/loginjwt/LoginJwtApplication.java
+++ login-jwt/src/main/java/com/clear/loginjwt/LoginJwtApplication.java
@@ -2,11 +2,12 @@ package com.clear.loginjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author jiexipeng
  */
-@SpringBootApplication
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class})
 public class LoginJwtApplication {
 
     public static void main(String[] args) {
